af25453e44179b7b0a00527c8d0817a7090f2ee4
Park so-yeon <thdusdl1219@postech.ac.kr>
2014-11-29 01:03:35 +0900

    끝나는 것까지.

--- src/scene/endgamescene.cpp
+++ src/scene/endgamescene.cpp
@@ -1,5 +1,6 @@
 #include "endgamescene.h"
-
+#include "../map.h"
+#include "../character.h"
 EndgameScene::EndgameScene(QObject *parent) :
   Scene(parent)
 {
@@ -15,10 +16,36 @@ void EndgameScene::setupEndgame()
 {
 	Qneed* background = new Qneed(this, get_window());
 	background->loadImage(":images/endgame/endgame_background.png");
-	set_background(background);
+    set_background(background);
+
+    win = new Qneed(this, get_window());
+    win->setZValue(10);
+    Character* player1 = Map::get_map()->player1;
+    Character* player2 = Map::get_map()->player2;
+    if(player1->getLife() == 0 && player2->getLife() == 0)
+        win->loadImage(":images/endgame/endgame_draw.png");
+    else if(player1->getLife() == 0)
+        win->loadImage(":images/endgame/endgame_kaist.png");
+    else
+        win->loadImage(":images/endgame/endgame_postech.png");
 }
 
 void EndgameScene::action()
+{
+
+}
+
+OnemoreButton::OnemoreButton(QGraphicsScene *scene, MainWindow *window) : Qneed(scene, window)
+{
+
+}
+
+void OnemoreButton::mousePressEvent(QGraphicsSceneMouseEvent *e)
+{
+
+}
+
+void OnemoreButton::mouseReleaseEvent(QGraphicsSceneMouseEvent* e)
 {
 
 }
--- src/scene/endgamescene.h
+++ src/scene/endgamescene.h
@@ -2,7 +2,7 @@
 #define ENDGAMESCENE_H
 
 #include "scene.h"
-
+#include "../qneed.h"
 class EndgameScene : public Scene
 {
   Q_OBJECT
@@ -14,7 +14,16 @@ public:
 signals:
 
 public slots:
+private:
+    Qneed* win;
+};
 
+class OnemoreButton : public Qneed
+{
+public:
+    OnemoreButton(QGraphicsScene*, MainWindow*);
+    virtual void mousePressEvent(QGraphicsSceneMouseEvent*);
+    virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent*);
 };
 
 #endif // ENDGAMESCENE_H
