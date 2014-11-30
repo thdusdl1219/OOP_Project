39f0fba22c2a7204757421974ab7ebe407db2c7e
HyungSeokChoi <hse9403@postech.ac.kr>
2014-11-30 16:59:11 +0900

    draw상황 빼고 대충 endgame만들어봄 그리고 endgamescene.cpp 수정했는데 이렇게 수정해도 될지좀 봐줘

--- src/scene/endgamescene.cpp
+++ src/scene/endgamescene.cpp
@@ -17,24 +17,25 @@ EndgameScene::~EndgameScene()
 void EndgameScene::setupEndgame()
 {
 	Qneed* background = new Qneed(this, get_window());
-	background->loadImage(":images/endgame/endgame_background.png");
-    set_background(background);
+    //background->loadImage(":images/endgame/endgame_background.png");
+    //set_background(background);
 
-    win = new Qneed(this, get_window());
-    win->setZValue(10);
+   // win = new Qneed(this, get_window());
+   // win->setZValue(10);
     Character* player1 = Map::get_map()->player1;
     Character* player2 = Map::get_map()->player2;
     if(player1->getLife() == 0 && player2->getLife() == 0)
-        win->loadImage(":images/endgame/endgame_draw.png");
+        background->loadImage(":images/endgame/endgame_draw.png");
     else if(player1->getLife() == 0)
-        win->loadImage(":images/endgame/endgame_kaist.png");
+        background->loadImage(":images/endgame/endgame_kaist.png");
     else
-        win->loadImage(":images/endgame/endgame_postech.png");
-    win->setPos(10,10);
+        background->loadImage(":images/endgame/endgame_postech.png");
+    set_background(background);
+   // win->setPos(0,0);
     OnemoreButton* one = new OnemoreButton(this,get_window());
-    one->setPos(425, 528);
-    CreatorButton* cre = new CreatorButton(this, get_window());
-    cre->setPos(425, 624);
+    one->setPos(824, 553);
+    CreatorButton* cre = new CreatorButton(this,get_window());
+    cre->setPos(824, 649);
 }
 
 void EndgameScene::action()
