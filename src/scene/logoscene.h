84cd252356cf1cbf79d825227288a1de297cf014
Park so-yeon <thdusdl1219@postech.ac.kr>
2014-11-30 18:59:11 +0900

    logo 화면 수정

--- src/scene/logoscene.h
+++ src/scene/logoscene.h
@@ -15,18 +15,10 @@ public:
     ~LogoScene();
 private:
     Qneed* logo;
-};
 
-
-class InitButton : public Qneed
-{
-    Q_OBJECT
-public:
-    InitButton(QGraphicsScene*, MainWindow*);
-    virtual void mousePressEvent(QGraphicsSceneMouseEvent*);
-    virtual void mouseReleaseEvent(QGraphicsSceneMouseEvent*);
 public slots:
     void showbutton();
 };
+
 
 #endif // LOGOSCENE_H
