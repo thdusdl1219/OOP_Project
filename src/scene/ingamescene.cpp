2d30dad8b92464a8676ce00cc98732501e5f8d9d
HyungSeokChoi <hse9403@postech.ac.kr>
2014-12-02 21:16:15 +0900

    필요없는거 이미지 지움

--- src/scene/ingamescene.cpp
+++ src/scene/ingamescene.cpp
@@ -16,6 +16,10 @@ InGameScene::InGameScene(QObject *parent) :
 	player2 = ChoiceScene :: get_ChoiceScene()->player2;
 	keyUp = keyDown = keyLeft = keyRight = false;
     keyT = keyF = keyG = keyH = false;
+<<<<<<< HEAD
+=======
+    //keyW = keyA = keyS = keyD = false;
+>>>>>>> 필요없는거 이미지 지움
     bomb1 = bomb2 = false;
 	setupIngame();
     scene = this;
